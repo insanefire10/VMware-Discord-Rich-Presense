f1d7d43fe28129c6b63e2da298421c475d4eb92a
Adit Rajkumar <36336394+insanefire10@users.noreply.github.com>
2024-02-23 14:37:20 -0500

    More stable release

--- vmwplugin.py
+++ vmwplugin.py
@@ -4,25 +4,43 @@ import psutil
 import pyautogui
 import random
 
-#enter your discord client ID as in instructions
-client_id = ""
-
-def update():
-    OSString = getGuestOS()
-    OSlogo = OSimage(OSString)
-    if OSlogo == "kali":
-        vmstate = "Hacking🤓" 
-    else:
-        vmstate = detailJokeList[random.randint(0,len(detailJokeList)-1)]
-    RPC.update(
-        state=vmstate,
-        large_image=OSlogo,
-        details=OSString
-    )
-
+#detect if VMware/Discord is active
 def vmdetect():
     return "vmware.exe" in (i.name() for i in psutil.process_iter()) 
 
+def discorddetect():
+    return "Discord.exe" in (i.name() for i in psutil.process_iter())
+
+#Update Discord Status
+def update():
+    OSString = getGuestOS()
+    OSlogo = OSimage(OSString)
+
+    if OSlogo == "kali":
+        vmstate = "Hacking🤓"
+    elif OSlogo == "ubuntu":
+        vmstate = "Deleting /etc/passwd"
+    else:
+        vmstate = detailJokeList[random.randint(0,len(detailJokeList)-1)]
+    
+    if OSlogo == "vmwarelogo":
+        minilogo = " "
+    else: 
+        minilogo = "vmwarelogo"
+
+    try:
+        RPC.update(
+            state=vmstate,
+            large_image=OSlogo,
+            details=OSString,
+            small_image=minilogo
+        )
+    except:
+        print("Connection Error: Cannot Update, API error")
+        return
+        
+
+#Get VMware Data
 def getGuestOS():
     windows = pyautogui.getAllWindows()
     for window in windows:
@@ -38,42 +56,62 @@ def OSimage(osname):
 #List Objects
 OSDict = {"Windows 10":"win10",
           "Windows 11":"win11",
-          "Kali":"kali",
-          "Ubuntu":"ubuntu",
+          "Windows 8":"win8",
           "Windows 7":"win7",
           "Windows Vista":"winvista",
-          "Windows XP":"winxp"}
+          "Windows XP":"winxp",
+          "Windows":"win7",
+          "Kali":"kali",
+          "Ubuntu":"ubuntu"}
 
-detailJokeList = ("Deleting System32", "Messing with Registry Keys", "Another Day another BSOD", "Alt + F4", "Sending Microsoft my data", "I have your SMB")
+detailJokeList = ("Deleting System32",
+                  "Messing with Registry Keys",
+                  "Another Day another BSOD",
+                  "Alt + F4", "Sending Microsoft my data",
+                  "I have your SMB",
+                  "Dodging OneDrive")
 
 #main
 
-#Checking if Discord is running
-if not "Discord.exe" in (i.name() for i in psutil.process_iter()):
-    print("Discord is not connected")
-    exit()
+#Enter Client ID here
+client_id = ""
+time.sleep(25)
 
-#Creating connection
-RPC = Presence(client_id)
-RPC.connect()
+flagCon = False
+flagIsUpdated = True
+flagVMw = False
 
-flagOff = True
 print("Running")
 while True:
-    if not "Discord.exe" in (i.name() for i in psutil.process_iter()):
+    time.sleep(5)
+    if not discorddetect():
         print("Discord Not Detected")
-        exit()
-    time.sleep(4)
+        if flagCon == True:
+            RPC.close()
+        flagCon = False
+        continue
+    
     if vmdetect():
+        if flagVMw == False:
+            RPC = Presence(client_id)
+            try:
+                RPC.connect()
+            except:
+                print("Connection Error")
+                continue
+            flagVMw = True
+            flagCon = True
         update()
         print("Detected")
-        flagOff = False
+
     else:
         print("VMW not detect")
-        if flagOff:
-            continue
-        else:
+        if flagVMw == True:
+            flagVMw = False
+            flagCon = False 
             RPC.clear()
-            flagOff = True
-    
+            RPC.close()
+        flagVMw = False
+        flagCon = False
+
 print("Exited")
