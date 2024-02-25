1971b905b4fe5dd0d09d4f36868c398c1048106e
Adit Rajkumar <36336394+insanefire10@users.noreply.github.com>
2024-02-24 22:48:11 -0500

    V1.3

--- vmwplugin.py
+++ vmwplugin.py
@@ -4,6 +4,9 @@ import psutil
 import pyautogui
 import random
 
+#Enter Client ID here
+client_id = ""
+
 #detect if VMware/Discord is active
 def vmdetect():
     return "vmware.exe" in (i.name() for i in psutil.process_iter()) 
@@ -17,7 +20,7 @@ def update():
     OSlogo = OSimage(OSString)
 
     if OSlogo == "kali":
-        vmstate = "Hacking🤓"
+        vmstate = "Found your web server"
     elif OSlogo == "ubuntu":
         vmstate = "Deleting /etc/passwd"
     else:
@@ -26,14 +29,15 @@ def update():
     if OSlogo == "vmwarelogo":
         minilogo = " "
     else: 
-        minilogo = "vmwarelogo"
+        minilogo = "https://imgur.com/bXH4oFE.png"
 
     try:
         RPC.update(
             state=vmstate,
             large_image=OSlogo,
             details=OSString,
-            small_image=minilogo
+            small_image=minilogo,
+            start=time.time()
         )
     except:
         print("Connection Error: Cannot Update, API error")
@@ -51,35 +55,35 @@ def OSimage(osname):
     for key, value in OSDict.items():
         if key in osname:
             return value
-    return "vmwarelogo"
+    return "https://imgur.com/bXH4oFE.png"
 
 #List Objects
-OSDict = {"Windows 10":"win10",
-          "Windows 11":"win11",
-          "Windows 8":"win8",
-          "Windows 7":"win7",
-          "Windows Vista":"winvista",
-          "Windows XP":"winxp",
-          "Windows":"win7",
-          "Kali":"kali",
-          "Ubuntu":"ubuntu"}
+OSDict = {"Windows 10":"https://imgur.com/kxO5E9i.png",
+          "Windows 11":"https://imgur.com/o8kY5or.png",
+          "Windows 8":"https://imgur.com/FvYjSTf.png",
+          "Windows 7":"https://imgur.com/L77FPCt.png",
+          "Windows Vista":"https://imgur.com/L77FPCt.png",
+          "Windows XP":"https://imgur.com/L77FPCt.png",
+          "Windows":"https://imgur.com/L77FPCt.png",
+          "Kali":"https://imgur.com/R1WBgCS.png",
+          "Ubuntu":"https://imgur.com/Woznh88.png"}
 
 detailJokeList = ("Deleting System32",
                   "Messing with Registry Keys",
                   "Another Day another BSOD",
-                  "Alt + F4", "Sending Microsoft my data",
+                  "Alt + F4", 
+                  "Sending Microsoft my data",
                   "I have your SMB",
                   "Dodging OneDrive")
 
 #main
 
-#Enter Client ID here
-client_id = ""
 time.sleep(25)
 
 flagCon = False
 flagIsUpdated = True
 flagVMw = False
+detectVMChange = " "
 
 print("Running")
 while True:
@@ -92,6 +96,9 @@ while True:
         continue
     
     if vmdetect():
+        if detectVMChange == getGuestOS():
+            continue
+        detectVMChange = getGuestOS()
         if flagVMw == False:
             RPC = Presence(client_id)
             try:
